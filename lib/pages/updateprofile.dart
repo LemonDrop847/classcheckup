6f3b0ddfae95184ef5c68e705a6fddb547d85e24
LemonDrop847 <nitinmishra847@gmail.com>
2023-05-31 08:01:42 +0530

    Update Profile

---
+++ lib/pages/updateprofile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateProfile extends StatefulWidget {
+  const UpdateProfile({super.key});
+
+  @override
+  State<UpdateProfile> createState() => _UpdateProfileState();
+}
+
+class _UpdateProfileState extends State<UpdateProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
